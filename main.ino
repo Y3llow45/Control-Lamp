4a2281f413943fda8092b46b37b23dac98b18232
Borky <68009977+Y3llow45@users.noreply.github.com>
2022-08-17 09:53:26 +0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+SoftwareSerial HM10(0, 1); // RX = 2, TX = 3
+char appData;  
+String inData = "";
+
+void setup()
+{
+  Serial.begin(9600);
+  HM10.begin(9600);
+  pinMode(8, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+void loop()
+{
+  HM10.listen();
+  while (HM10.available() > 0) {
+    appData = HM10.read();
+    inData = String(appData);
+    Serial.print(appData);
+    Serial.write(appData);
+  }
+  if (Serial.available()) {
+    delay(10);
+    HM10.write(Serial.read());
+  }
+  if ( inData == "A") {
+    Serial.println("LAMP ON");
+    digitalWrite(8, HIGH);
+    delay(100);
+  }
+  if ( inData == "B") {
+    Serial.println("LAMP OFF");
+    digitalWrite(8, LOW);
+    delay(100);
+  }
+}
